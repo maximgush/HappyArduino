a148f65f1f484fa42dfcd97eec1068210c463f26
julia ivanovaa <ivanova.yl.k@gmail.com>
2019-09-28 12:51:18 +0300

    Заготовка для DHT11TempSensorComponent (недоделана, нетестирована) HappyHidroponics.ino заменено на более актуальный скетч автополива

---
+++ HappyArduinoLib/DHT11TempSensorComponent.h
@@ -0,0 +1,58 @@
+﻿#pragma once
+
+#include "Component.h"
+#include "DHT.h"
+
+enum eDHTType
+{
+	edtDHT11,
+	edtDHT22,
+	edtDHT21
+};
+
+class DHT11TempSensorComponent : public Component
+{
+private:
+	short pin;
+	float humidity;
+	float temperature;
+
+	DHT dht;
+public:
+	DHT11TempSensorComponent (const char* _name, short _pin, eDHTType dhtType)
+		: Component(_name)
+		, pin(_pin)
+		, dht(pin, dhtType)
+		, temperature(-273)
+		, humidity(-1)
+	{				
+	}
+
+	virtual void Init() override
+	{
+		dht.begin();
+	}
+	
+	virtual void OnFrame() override
+	{
+		humidity = dht.readHumidity();
+  		temperature = dht.readTemperature();
+	};
+	
+	virtual void GetKeyValues(KeyValue *keyValues, short &size) override
+	{
+		keyValues[size] = KeyValue("Температура", String(temperature, DEC) + " °С"); size++;
+		keyValues[size] = KeyValue("Влажность", String(temperature, DEC) + " %"); size++;
+		keyValues[size] = KeyValue("pin", String(pin, DEC)); size++;
+	};
+	
+	float GetTemperature()
+	{
+		return temperature;
+	}
+
+	float GetHumidity()
+	{
+		return humidity;
+	}
+};
--- Projects/HappyHydroponics/HappyHydroponics.ino
+++ Projects/HappyHydroponics/HappyHydroponics.ino
@@ -1,208 +1,172 @@
-/*
-   Copyright (c) 2015, Majenko Technologies
-   All rights reserved.
+#include "E:\Programming\Arduino\HappyArduinoLib\HappyArduinoLib\ESP8266WebServerComponent.h"
+#include "E:\Programming\Arduino\HappyArduinoLib\HappyArduinoLib\RTCComponent.h"
+#include "E:\Programming\Arduino\HappyArduinoLib\HappyArduinoLib\RelayComponent.h"
+#include "E:\Programming\Arduino\HappyArduinoLib\HappyArduinoLib\List.h"
 
-   Redistribution and use in source and binary forms, with or without modification,
-   are permitted provided that the following conditions are met:
+ESP8266WebServerComponent WebServer("ESP8266WebServer","MGTS_GPON_8044","NHBK4MKH",80);
+RTCComponent rtcComponent("RTC_DS3231");
+RelayComponent relay("Relay_On_Pump", 2);
 
- * * Redistributions of source code must retain the above copyright notice, this
-     list of conditions and the following disclaimer.
-
- * * Redistributions in binary form must reproduce the above copyright notice, this
-     list of conditions and the following disclaimer in the documentation and/or
-     other materials provided with the distribution.
-
- * * Neither the name of Majenko Technologies nor the names of its
-     contributors may be used to endorse or promote products derived from
-     this software without specific prior written permission.
-
-   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-   ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-   WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-   DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
-   ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-   (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-   LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
-   ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
-
-#include <ESP8266WiFi.h>
-#include <WiFiClient.h>
-#include <ESP8266WebServer.h>
-#include <ESP8266mDNS.h>
-
-#include <RTClib.h>
-#include <Wire.h>
-
-#ifndef STASSID
-#define STASSID "MGTS_GPON_8044"
-#define STAPSK  "NHBK4MKH"
-#endif
-
-const char *ssid = STASSID;
-const char *password = STAPSK;
-
-#define PIN_RELAY 13
-
-RTC_DS3231 rtc;
-
-
-struct sData
+class ScheduledEventManagerComponent : Component
 {
-  int Year, Month, Day;
-  int Hour, Minute, Second;
-  int Temperature;
-  bool RelayOn;
-} data;
-char t[32];
+public:
+  struct TimerEvent
+  {    
+    // Время наступления события
+    DateTime dateTime;
 
-ESP8266WebServer server(80);
+    // Описание события
+    String description;
 
-const int led = 14;
-
-void handleRoot() {
-  digitalWrite(led, 1);
-  char temp[400];
-
-  snprintf(temp, 400,
-
-           "<html>\
-  <head>\
-    <meta http-equiv='refresh' content='5'/>\
-    <title>ESP8266 Demo</title>\
-    <style>\
-      body { background-color: #cccccc; font-family: Arial, Helvetica, Sans-Serif; Color: #000088; }\
-    </style>\
-  </head>\
-  <body>\
-    <h1>Hello from ESP8266!</h1>\
-    <p>CurrentDate: %02d.%02d.%04d</p>\
-    <p>CurrentTime: %02d:%02d:%02d</p>\
-    <p>Temperature: %02d C</p>\
-    <p>RelayOn: %01d</p>\
-    <img src=\"/test.svg\" />\
-  </body>\
-</html>",
-           data.Day, data.Month, data.Year,
-           data.Hour, data.Minute, data.Second,
-           data.Temperature,
-           data.RelayOn
-          );
-  server.send(200, "text/html", temp);
-  digitalWrite(led, 0);
-}
-
-void handleNotFound() {
-  digitalWrite(led, 1);
-  String message = "File Not Found\n\n";
-  message += "URI: ";
-  message += server.uri();
-  message += "\nMethod: ";
-  message += (server.method() == HTTP_GET) ? "GET" : "POST";
-  message += "\nArguments: ";
-  message += server.args();
-  message += "\n";
-
-  for (uint8_t i = 0; i < server.args(); i++) {
-    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
-  }
-
-  server.send(404, "text/plain", message);
-  digitalWrite(led, 0);
-}
-
-void setup(void) {
-   Wire.begin(); 
-  rtc.begin();
-  rtc.adjust(DateTime(F(__DATE__),F(__TIME__)));
-
-  #ifndef ESP8266
-  while (!Serial); // for Leonardo/Micro/Zero
-#endif
-
-  Serial.begin(115200);
-
-  delay(3000); // wait for console opening
+    // Конструктор
+    TimerEvent(DateTime _dateTime, const char* _description) : dateTime(_dateTime), description(_description) {}
   
-  Serial.begin(115200);
-  Serial.println(":::");
+    // Функция которое вызывается при наступлении события
+    virtual void OnEvent() {};
+  };  
+
+private:  
+  RTCComponent* rtcComponent;
+  List<TimerEvent*> futureEvents;
+  List<TimerEvent*> pastEvents;
+
+public:
+  ScheduledEventManagerComponent(const char* _name, RTCComponent * _rtcComponent)
+    : Component(_name), rtcComponent(_rtcComponent) {};  
   
-  pinMode(13, OUTPUT);  
-  digitalWrite(13, HIGH);
-  digitalWrite(led, 0);
-  Serial.begin(115200);
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  Serial.println("");
+  virtual void OnFrame()
+  {
+    DateTime now = rtcComponent->rtc.now();
+    List<TimerEvent*>::ListNode* firstFutureEvent = futureEvents.First();
+    while (firstFutureEvent != NULL && (*firstFutureEvent)->dateTime < now)
+    {      
+      (*firstFutureEvent)->OnEvent();
+      futureEvents.erase(firstFutureEvent);
+      pastEvents.push_back(firstFutureEvent);
+      firstFutureEvent = futureEvents.First();
+    }
+  }
+  
+  virtual void GetKeyValues(KeyValue *keyValues, short &size)
+  {    
+    List<TimerEvent*>::ListNode* eventNode = pastEvents.First();
+    while (eventNode != NULL)
+    {      
+      keyValues[size] = KeyValue((*eventNode)->description, "Наступило"); size++;      
+      eventNode = eventNode->Next();
+    }
 
-  // Wait for connection
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+    eventNode = futureEvents.First();
+    while (eventNode != NULL)
+    {      
+      keyValues[size] = KeyValue((*eventNode)->description, "Ожидается"); size++;      
+      eventNode = eventNode->Next();
+    }
   }
 
-  Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(ssid);
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
-
-  if (MDNS.begin("esp8266")) {
-    Serial.println("MDNS responder started");
+  void AddEvent(TimerEvent * newEvent)
+  {
+    DateTime now = rtcComponent->rtc.now();
+    if (now < newEvent->dateTime)    
+    {
+      List<TimerEvent*>::ListNode* eventNode = futureEvents.First();
+      if (eventNode == NULL)
+        futureEvents.push_front(newEvent);
+      else
+      {
+        while (eventNode != NULL && (*eventNode)->dateTime < newEvent->dateTime )           
+          eventNode = eventNode->Next();
+          
+        if (eventNode == NULL)
+          futureEvents.push_back(newEvent);
+        else
+          futureEvents.insert(newEvent, eventNode);
+      }
+    }
+    else
+    {
+      pastEvents.push_back(newEvent);
+    }     
   }
+};
 
-  server.on("/", handleRoot);
-  //server.on("/test.svg", drawGraph);
-  server.on("/inline", []() {
-    server.send(200, "text/plain", "this works as well");
-  });
-  server.onNotFound(handleNotFound);
-  server.begin();
-  Serial.println("HTTP server started");
+ScheduledEventManagerComponent eventManager("EventManager", &rtcComponent);
+
+struct RelayOnTimerEvent : public ScheduledEventManagerComponent::TimerEvent
+{
+  RelayOnTimerEvent(DateTime _dateTime, const char* _description)
+     : TimerEvent(_dateTime,_description) {}
+  
+  virtual void OnEvent()
+  {
+    relay.On();
+    Serial.println("-------> Relay On");
+  };
+};
+
+struct RelayOffTimerEvent : public ScheduledEventManagerComponent::TimerEvent
+{
+  RelayOffTimerEvent(DateTime _dateTime, const char* _description)
+     : TimerEvent(_dateTime,_description) {}
+     
+  virtual void OnEvent()
+  {
+    relay.Off();
+    Serial.println("-------> Relay Off");
+  };
+};
+
+void CreateAutoWateringEvent(const char* _date, const char* _time, unsigned short _durationSec)
+{ 
+  DateTime dateTimeOn(_date,_time);  
+  char descr[100];
+  sprintf(descr, "Автоматический полив %02u.%02u.%04u %02u:%02u:%02u. Начало", dateTimeOn.day(), dateTimeOn.month(), dateTimeOn.year(), dateTimeOn.hour(), dateTimeOn.minute(), dateTimeOn.second());
+  eventManager.AddEvent(new RelayOnTimerEvent(dateTimeOn,  descr));
+
+  DateTime dateTimeOff = dateTimeOn + TimeSpan(_durationSec);
+  sprintf(descr, "Автоматический полив %02u.%02u.%04u %02u:%02u:%02u. Конец", dateTimeOff.day(), dateTimeOff.month(), dateTimeOff.year(), dateTimeOff.hour(), dateTimeOff.minute(), dateTimeOff.second());
+  eventManager.AddEvent(new RelayOffTimerEvent(dateTimeOff, descr));
 }
 
-void loop(void) {
-  DateTime now = rtc.now();  
-
-  data.Year = now.year();
-  data.Month = now.month();
-  data.Day = now.day();
-  data.Hour = now.hour();
-  data.Minute = now.minute();
-  data.Second = now.second();
-  data.Temperature = rtc.getTemperature();
-  data.RelayOn = true;
-  //sprintf(t, "%02d:%02d:%02d %02d/%02d/%02d",  now.hour(), now.minute(), now.second(), now.day(), now.month(), now.year());.
-  //Serial.println(t);
-
-  if (data.Second / 6 % 2 == 0)
-    data.RelayOn = HIGH;
-  else
-    data.RelayOn = LOW;
-  delay(5000);
-
-  digitalWrite(PIN_RELAY, data.RelayOn);
+void setup(){ 
+  Serial.begin(115200);     // Communication started with 9600 baud
   
-  server.handleClient();
-  MDNS.update();
+  ComponentsContainer::Instance().Init(); 
+/*
+  {
+    // Test
+    DateTime now = rtcComponent.rtc.now();    
+    char date[11], time[8];
+    for (unsigned short i = 2; i < 3; ++i)
+    {
+      if (now.minute() + i >= 60)
+        continue;
+
+      DateTime dateTime = now + TimeSpan(i * 60);      
+      sprintf(date, "Sep %02d %04d", dateTime.day(), dateTime.year());
+      sprintf(time, "%02d:%02d:%02d", dateTime.hour(), dateTime.minute(), dateTime.second());
+      CreateAutoWateringEvent(date, time, 90); // Вс
+    }
+  }*/
+
+  // TEST
+  CreateAutoWateringEvent("Sep 08 2019", "00:13:00", 10); // Вс
+  
+  CreateAutoWateringEvent("Sep 08 2019", "09:00:00", 90); // Вс
+  CreateAutoWateringEvent("Sep 12 2019", "13:00:00", 90); // Чт
+  CreateAutoWateringEvent("Sep 16 2019", "13:00:00", 90); // Пн
+  CreateAutoWateringEvent("Sep 19 2019", "13:00:00", 90); // Чт
 }
 
-void drawGraph() {
-  String out = "";
-  char temp[100];
-  out += "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"400\" height=\"150\">\n";
-  out += "<rect width=\"400\" height=\"150\" fill=\"rgb(250, 230, 210)\" stroke-width=\"1\" stroke=\"rgb(0, 0, 0)\" />\n";
-  out += "<g stroke=\"black\">\n";
-  int y = rand() % 130;
-  for (int x = 10; x < 390; x += 10) {
-    int y2 = rand() % 130;
-    sprintf(temp, "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke-width=\"1\" />\n", x, 140 - y, x + 10, 140 - y2);
-    out += temp;
-    y = y2;
-  }
-  out += "</g>\n</svg>\n";
+void loop(){
+  ComponentsContainer::Instance().OnFrame();
 
-  server.send(200, "image/svg+xml", out);
+  {
+    DateTime now = rtcComponent.rtc.now();
+    char time[8];
+    sprintf(time, "%02d:%02d:%02d", now.hour(), now.minute(), now.second());
+    Serial.println(time);
+  }  
+
+  delay(1000);
 }
